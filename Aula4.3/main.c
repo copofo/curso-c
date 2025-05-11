ce850adc921802fd58d913b56206939ae1bec130
copofo <121464011+copofo@users.noreply.github.com>
2025-05-11 17:49:47 -0300

    next

---
+++ Aula4.3/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*--------------------------------
+
+* Comando switch-case:
+    * É como se fossem vários if em sequência
+    * Útil para comparar uma única variável
+    * Somente comparações de igualdade
+* Caso seja igual a 1 dos valores:
+    * Executa o respectivo bloco
+    * Ignora todos os outros blocos
+
+
+
+
+--------------------------------*/
+
+int main(){
+    printf("\nAula 4.3\n");
+}
+
